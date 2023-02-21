2e5871fafccb72259696071001bde19cff1988a9
MARIUS SERTVYTIS <marius.sert@gmail.com>
2023-02-21 23:15:22 +0200

    lesson_12

--- 02.20/second.py
+++ 02.20/second.py
@@ -1,15 +1,17 @@
 def divide(a: int, b: int) -> float:
     try:
         result = a / b
+        return result
     except ZeroDivisionError:
         print("Error: division by zero!")
         result = None
     except Exception as e:
         print(f"Error: {e}")
         result = None
-    else:
-        print("Division succeeded!")
     finally:
         print(result)
 
-divide(5, 1)
+divide(10, 0)
+
+
+
